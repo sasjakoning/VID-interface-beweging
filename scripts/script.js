c0fdfc752403cd2aed38e752b49260b81e8145d4
sasjakoning <sasjakoning@hotmail.com>
2022-12-06 18:04:01 +0100

    added: more animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,53 @@
-// JavaScript Document
-console.log("hi");
 
 const btnPlay = document.querySelector("#play");
 const btnCatch = document.querySelector("#catch");
+const btnWalk = document.querySelector("#walk");
 const dogCanvas = document.getElementById("dogyCanvas");
 const loaderCanvas = document.getElementById("loaderCanvas");
+
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
+if(vw < 568 ){
+    console.log("small")
+    dogCanvas.width = window.innerWidth;
+    dogCanvas.height = window.innerWidth;
+}else if(vw >= 568 && vw < 992){
+    console.log("medium")
+    dogCanvas.width = window.innerWidth;
+    dogCanvas.height = window.innerWidth;
+}else if(vw >= 992){
+    console.log("large")
+    dogCanvas.width = 900;
+    dogCanvas.height = 900;
+}
+
 
 const dogyRive = new rive.Rive({
     src: "./images/dogy.riv",
     canvas: dogyCanvas,
     autoplay: true,
     stateMachines: 'dogy-states',
-    artboard: "Dogy",
+    artboard: "dogy",
     fit: rive.Fit.cover,
     onLoad: (_) => {
         const inputs = dogyRive.stateMachineInputs("dogy-states")
-        const triggerPlay = inputs.find(i => i.name === "Play")
+        const triggerPlay = inputs.find(i => i.name === "play")
         btnPlay.onclick = (e) => {
             e.preventDefault();
-            console.log("hellooo")
             triggerPlay.fire()
         }
 
-        const triggerCatch = inputs.find(i => i.name === "Catch")
+        const triggerCatch = inputs.find(i => i.name === "catch")
         btnCatch.onclick = (e) => {
             e.preventDefault();
-            console.log("hellooo")
             triggerCatch.fire()
+        }
+
+        const triggerWalk = inputs.find(i => i.name === "walk")
+        btnWalk.onclick = (e) => {
+            console.log("sadas")
+            e.preventDefault();
+            triggerWalk.fire()
         }
     }
 });
