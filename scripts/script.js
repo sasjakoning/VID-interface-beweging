dff44030ef08f0c2960c75ae5ae734c102fc1e62
sasjakoning <sasjakoning@hotmail.com>
2022-12-07 17:02:50 +0100

    added animated icons and what not

--- scripts/script.js
+++ scripts/script.js
@@ -3,29 +3,11 @@ const btnPlay = document.querySelector("#play");
 const btnCatch = document.querySelector("#catch");
 const btnWalk = document.querySelector("#walk");
 const dogCanvas = document.getElementById("dogyCanvas");
-const loaderCanvas = document.getElementById("loaderCanvas");
+const animContainer = document.querySelector(".animContainer")
+
 
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 
-// window.addEventListener("resize", ()=> {
-//     if(vw < 568 ){
-//         console.log("small")
-//         dogyRive.resizeToCanvas()
-//         dogCanvas.width = window.innerWidth;
-//         dogCanvas.height = window.innerWidth;
-//     }else if(vw >= 568 && vw < 992){
-//         console.log("medium")
-//         dogyRive.resizeToCanvas()
-//         dogCanvas.width = window.innerWidth;
-//         dogCanvas.height = window.innerWidth;
-//     }else if(vw >= 992){
-//         console.log("large")
-//         // dogyRive.resizeToCanvas()
-//         dogCanvas.width = 900;
-//         dogCanvas.height = 900;
-//     }
-
-// })
 
 window.addEventListener("resize", () => {
     dogyRive.resizeDrawingSurfaceToCanvas();
@@ -40,7 +22,6 @@ const dogyRive = new rive.Rive({
     artboard: "dogy",
     fit: rive.Fit.cover,
     onLoad: (_) => {
-        dogyRive.resizeDrawingSurfaceToCanvas();
 
         const inputs = dogyRive.stateMachineInputs("dogy-states")
         const triggerPlay = inputs.find(i => i.name === "play")
@@ -61,18 +42,65 @@ const dogyRive = new rive.Rive({
 
             if(triggerWalk.value == false) {
                 triggerWalk.value = true
+                animContainer.classList.add("backgroundAnim")
             }else {
                 triggerWalk.value = false
+                animContainer.classList.remove("backgroundAnim")
             }
         }
     }
 });
  
-const loaderRive = new rive.Rive({
+// const loaderRive = new rive.Rive({
+//     src: "./images/dogy.riv",
+//     canvas: loaderCanvas,
+//     autoplay: true,
+//     stateMachines: 'loader-states',
+//     artboard: "loader",
+//     fit: rive.Fit.cover,
+// });
+
+const loaderCanvas = document.getElementById("loaderCanvas");
+const btnPlayCanvas = document.getElementById("btnPlayCanvas");
+const BtnCatchCanvas = document.getElementById("btnCatchCanvas");
+const BtnWalkCanvas = document.getElementById("btnWalkCanvas");
+
+
+const btnPlayRive = new rive.Rive({
     src: "./images/dogy.riv",
-    canvas: loaderCanvas,
+    canvas: btnPlayCanvas,
     autoplay: true,
-    stateMachines: 'loader-states',
-    artboard: "loader",
+    stateMachines: 'icon-play-states',
+    artboard: "icon-play",
     fit: rive.Fit.cover,
+    onLoad: (_) => {
+
+        btnPlayRive.resizeDrawingSurfaceToCanvas();
+    }
+});
+
+const btnCatchRive = new rive.Rive({
+    src: "./images/dogy.riv",
+    canvas: BtnCatchCanvas,
+    autoplay: true,
+    stateMachines: 'icon-catch-states',
+    artboard: "icon-catch",
+    fit: rive.Fit.cover,
+    onLoad: (_) => {
+
+        btnCatchRive.resizeDrawingSurfaceToCanvas();
+    }
+});
+
+const btnWalkRive = new rive.Rive({
+    src: "./images/dogy.riv",
+    canvas: BtnWalkCanvas,
+    autoplay: true,
+    stateMachines: 'icon-walk-states',
+    artboard: "icon-walk",
+    fit: rive.Fit.cover,
+    onLoad: (_) => {
+
+        btnWalkRive.resizeDrawingSurfaceToCanvas();
+    }
 });
